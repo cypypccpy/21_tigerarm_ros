bdf151917f07a5b02141fc79b45403448b1cc2bb
Your Name <you@example.com>
2021-06-23 18:39:50 +0800

    在减少sleep

--- src/21_tigerarm_ros.sh
+++ src/21_tigerarm_ros.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gnome-terminal -- bash -c "roslaunch pixle_godzilla_moveit_config demo.launch"
-sleep 4
+sleep 2
 gnome-terminal -- bash -c "roslaunch arm_moveit_kinematics demo.launch" &
 wait
 exit 0
