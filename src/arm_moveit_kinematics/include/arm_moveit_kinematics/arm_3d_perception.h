034a37ee215fbeccd4cfba7f8c091036c958552d
cypypccpy <2415108441@qq.com>
2021-07-09 16:43:05 +0800

    将mineral—detector整合进moveit

--- src/arm_moveit_kinematics/include/arm_moveit_kinematics/arm_3d_perception.h
+++ src/arm_moveit_kinematics/include/arm_moveit_kinematics/arm_3d_perception.h
@@ -1,4 +1,16 @@
 #include <ros/ros.h>
+#include <iostream>
+#include <sstream>
+#include <math.h>
+#include <algorithm> // std::min, std::max
+#include <tf/transform_broadcaster.h>
+#include "cv-helpers.hpp"
+#include "detect.h"
+#include "PlaneExtract.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/passthrough.h>
@@ -13,13 +25,19 @@ class Arm3DPerception {
     public:
         Arm3DPerception() {};
 
-        void cloudCB(const sensor_msgs::PointCloud2ConstPtr& input);
+        //void cloudCB(const sensor_msgs::PointCloud2ConstPtr& input);
         void imageCB(const sensor_msgs::PointCloud2ConstPtr& input);
+
+        void detect_mineral(const cv::Mat& src_img);
 
     private:
         bool points_not_found = true;
 
         ros::NodeHandle node_handle_;
+        ros::Publisher ui_puber;
         ros::Subscriber point_suber_;
         ros::Subscriber depth_suber_;
+
+        Object yolo_out;
+        detect yolo
 }
--- src/arm_moveit_kinematics/src/arm_3d_perception.cpp
+++ src/arm_moveit_kinematics/src/arm_3d_perception.cpp
@@ -1,31 +1,48 @@
 #include "arm_moveit_kinematics/arm_3d_perception.h"
 
-Arm3DPerception::Arm3DPerception() {
-  point_suber = node_handle_.subscribe("/camera/depth_registered/points", 1, &Arm3DPerception::imageCB, this);
-  depth_suber = node_handle_.subscribe("/camera/depth_registered/image_raw", 1, &Arm3DPerception::cloudCB, this);
+Arm3DPerception::Arm3DPerception():yolo("/home/robotlab/Desktop/MineralDetection/yolov3-tiny.cfg",
+                                  "/home/robotlab/Desktop/MineralDetection/yolov3-tiny_final.weights",
+                                  "/home/robotlab/Desktop/MineralDetection/OpenYolo/config/camPara.yml") {
+
+  //point_suber = node_handle_.subscribe("/camera/depth_registered/points", 1, &Arm3DPerception::imageCB, this);
+  depth_suber = node_handle_.subscribe("/camera/depth_registered/image_raw", 1, &Arm3DPerception::DepthCB, this);
 }
 
-void Arm3DPerception::cloudCB(const sensor_msgs::PointCloud2ConstPtr& input)
-  {
-    // BEGIN_SUB_TUTORIAL callback
-    //
-    // Perception Related
-    // ^^^^^^^^^^^^^^^^^^
-    // First, convert from sensor_msgs to pcl::PointXYZRGB which is needed for most of the processing.
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-    pcl::fromROSMsg(*input, *cloud);
-    
-    if (cloud->points.empty())
-    {
-      ROS_ERROR_STREAM_NAMED("cylinder_segment", "Can't find the cylindrical component.");
-      return;
-    }
-    if (points_not_found)
-    {
-      points_not_found = false;
-    }
-  }
+void Arm3DPerception::DepthCB(const sensor_msgs::PointCloud2ConstPtr& input) {
+  
+  
 };
+
+void detect_mineral(const cv::Mat& src_img) {
+  yolo_out = yolo.inference(src_img); //推理模型得到结果
+
+  if (yolo_out.boxes.size() <= 0)
+  {
+    continue;
+  }
+
+  sort(yolo_out.boxes.begin(), yolo_out.boxes.end(),
+        [](const Rect2i box1, const Rect2i box2)
+        {
+          return box1.area() > box2.area();
+        });
+  if (yolo_out.boxes[0].x <= 0)
+  {
+    yolo_out.boxes[0].x = 1;
+  }
+  if (yolo_out.boxes[0].y <= 0)
+  {
+    yolo_out.boxes[0].y = 1;
+  }
+  if (yolo_out.boxes[0].br().x >= 639)
+  {
+    yolo_out.boxes[0].width = 638 - yolo_out.boxes[0].x;
+  }
+  if (yolo_out.boxes[0].br().y >= 479)
+  {
+    yolo_out.boxes[0].height = 478 - yolo_out.boxes[0].y;
+  }
+}
 
 int main(int argc, char** argv)
 {
