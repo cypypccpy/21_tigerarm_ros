cea04b1a70bfe0e9d58f7bdc8800553e64d7f6e9
Your Name <you@example.com>
2021-07-09 20:40:27 +0800

    完全整合

--- src/arm_moveit_kinematics/include/arm_moveit_kinematics/arm_3d_perception.h
+++ src/arm_moveit_kinematics/include/arm_moveit_kinematics/arm_3d_perception.h
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <sstream>
 #include <math.h>
-#include <algorithm> // std::min, std::max
+#include <algorithm>
 #include <tf/transform_broadcaster.h>
 #include "cv-helpers.hpp"
 #include "detect.h"
@@ -10,34 +10,36 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <std_msgs/Int32.h>
 
 #include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/segmentation/sac_segmentation.h>
 
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit_msgs/CollisionObject.h>
 
 class Arm3DPerception {
     public:
-        Arm3DPerception() {};
+        explicit Arm3DPerception();
 
         //void cloudCB(const sensor_msgs::PointCloud2ConstPtr& input);
-        void imageCB(const sensor_msgs::PointCloud2ConstPtr& input);
+        void DepthCB(const sensor_msgs::ImageConstPtr& input);
 
-        void detect_mineral(const cv::Mat& src_img);
+        void key_recv_callback(const std_msgs::Int32& msg);
+
+        void detect_mineral(cv::Mat& src_img);
+
+        ~Arm3DPerception() {};
 
     private:
         bool points_not_found = true;
+        bool aim = false;
 
         ros::NodeHandle node_handle_;
-        ros::Publisher ui_puber;
+        image_transport::Publisher ui_puber_;
         ros::Subscriber point_suber_;
         ros::Subscriber depth_suber_;
+        ros::Subscriber keyboard_suber_;
 
         Object yolo_out;
-        detect yolo
-}
+        detect yolo;
+};
--- src/arm_moveit_kinematics/src/arm_3d_perception.cpp
+++ src/arm_moveit_kinematics/src/arm_3d_perception.cpp
@@ -5,20 +5,42 @@ Arm3DPerception::Arm3DPerception():yolo("/home/robotlab/Desktop/MineralDetection
                                   "/home/robotlab/Desktop/MineralDetection/OpenYolo/config/camPara.yml") {
 
   //point_suber = node_handle_.subscribe("/camera/depth_registered/points", 1, &Arm3DPerception::imageCB, this);
-  depth_suber = node_handle_.subscribe("/camera/depth_registered/image_raw", 1, &Arm3DPerception::DepthCB, this);
+  depth_suber_ = node_handle_.subscribe("/camera/depth_registered/image_raw", 1, &Arm3DPerception::DepthCB, this);
+  keyboard_suber_ = node_handle_.subscribe("arm_keys", 100, &Arm3DPerception::key_recv_callback, this);
+
+  image_transport::ImageTransport it = image_transport::ImageTransport(node_handle_);
+  ui_puber_ = it.advertise("camera/image", 100);
 }
 
-void Arm3DPerception::DepthCB(const sensor_msgs::PointCloud2ConstPtr& input) {
+void Arm3DPerception::key_recv_callback(const std_msgs::Int32& msg) {
+  if (msg.data == 113) {
+    aim = true;
+  }
+  else
+    aim = false;
+}
+
+void Arm3DPerception::DepthCB(const sensor_msgs::ImageConstPtr& image_raw) {
+  cv::Mat src_img = cv_bridge::toCvShare(image_raw, "bgr8")->image;
   
+  //辅助对准矿石
+  if (aim == true) {
+    Rect r(212, 241, 294, 174);
+    rectangle(src_img, r, Scalar(0, 0, 255), 2);
+  }
+
+  //发布图片消息
+  sensor_msgs::ImagePtr msg1 = cv_bridge::CvImage(std_msgs::Header(), "bgr8", src_img).toImageMsg();
+  ui_puber_.publish(msg1);
   
 };
 
-void detect_mineral(const cv::Mat& src_img) {
+void Arm3DPerception::detect_mineral(cv::Mat& src_img) {
   yolo_out = yolo.inference(src_img); //推理模型得到结果
 
   if (yolo_out.boxes.size() <= 0)
   {
-    continue;
+    return;
   }
 
   sort(yolo_out.boxes.begin(), yolo_out.boxes.end(),
